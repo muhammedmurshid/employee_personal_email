ce4e8df5450ea8faf5d024a8631099bc4a9ed15e
Murshid <muhsink00000@gmail.com>
2023-07-11 16:40:00 +0530

    icon

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "Employee Personal Email",
+    'version': "14.0.1.0",
+    'sequence': "0",
+    'depends': ['base', 'mail', 'hr'],
+    'data': [
+       'views/personal_mail_employee.xml',
+    ],
+    'demo': [],
+    'summary': "email_for_employee",
+    'description': "this_is_my_module",
+    'installable': True,
+    'auto_install': False,
+    'license': "LGPL-3",
+    'application': False
+}
---
+++ models/personal_mail.py
@@ -0,0 +1,7 @@
+from odoo import models, fields, api
+
+
+class EmployeePersonalEmail(models.Model):
+    _inherit = "hr.employee"
+
+    personal_email = fields.Char(string='Personal Email', copy=False)

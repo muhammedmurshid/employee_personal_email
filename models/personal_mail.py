e765e17c199a5531952e488d30f59bf00db996ba
Murshid <muhsink00000@gmail.com>
2023-07-13 11:17:34 +0530

    Personal Email

--- models/personal_mail.py
+++ models/personal_mail.py
@@ -4,4 +4,4 @@ from odoo import models, fields, api
 class EmployeePersonalEmail(models.Model):
     _inherit = "hr.employee"
 
-    personal_email = fields.Char(string='Personal Email', copy=False)
+    personal_email = fields.Char(string='Personal Email')
